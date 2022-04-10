4d95a941373346100a528a30910000717e746467
Kanstantsin91 <56346923+Kanstantsin91@users.noreply.github.com>
2022-04-10 14:17:05 +0300

    12: profile, header, navbar

---
+++ src/components/Profile.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://cdn.cnn.com/cnnnext/dam/assets/181010131059-australia-best-beaches-cossies-beach-cocos3.jpg"
+                    alt="beach"/>
+            </div>
+            <div>ava+descr</div>
+            <div>my posts
+                <div>
+                    New posts
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
