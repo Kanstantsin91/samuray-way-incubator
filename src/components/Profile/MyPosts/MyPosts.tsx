56751b5d58137abecd9ab52c42a58555eebba68a
Kanstantsin91 <56346923+Kanstantsin91@users.noreply.github.com>
2022-04-12 23:30:24 +0300

    16: Post modified with props

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -9,9 +9,9 @@ const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div>
-                <Post/>
-                <Post/>
-                <Post/>
+                <Post message={"post1"} likeCount={15}/>
+                <Post message={"post2"} likeCount={20}/>
+                <Post message={"post3"}/>
             </div>
         </div>
     )
