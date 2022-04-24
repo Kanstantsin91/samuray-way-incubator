01f8989c0a030fe5637d67190187f350f6ead1d1
Kanstantsin91 <56346923+Kanstantsin91@users.noreply.github.com>
2022-04-24 12:25:14 +0300

    21-22: Code refactoring

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,11 +1,49 @@
 import React from "react";
 import classes from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
+
+type DialogItemType = {
+    id: number
+    name: string
+}
+
+
+const DialogItem = (props: DialogItemType) => {
+    const path = `/dialogs/${props.id}`;
+    return (
+        <div className={classes.dialog}>
+            <NavLink to={path}
+                     className={(navData) => navData.isActive ? classes.active : ''}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+type MessageType = {
+    message: string
+}
+
+const Message = (props: MessageType) => {
+    return <div className={classes.message}>{props.message}</div>
+}
 
 
 const Dialogs = () => {
-    return <div>
-        Dialogs
-    </div>
+    return (
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <DialogItem id={1} name={"Bill"}/>
+                <DialogItem id={2} name={"Bob"}/>
+                <DialogItem id={3} name={"Max"}/>
+                <DialogItem id={4} name={"Sasha"}/>
+                <DialogItem id={5} name={"Victor"}/>
+                <DialogItem id={6} name={"Peter"}/>
+            </div>
+            <div className={classes.messages}>
+                <Message message={"Hi"}/>
+                <Message message={"How are you?"}/>
+                <Message message={"Yo"}/>
+            </div>
+        </div>)
 }
 
 export default Dialogs;
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,16 +1,12 @@
 import React from "react";
-import classes from './Profile.module.css'
 import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
+
 
 const Profile = () => {
     return (
         <div>
-            <div>
-                <img className={classes.backgroundImg}
-                     src="https://cdn.cnn.com/cnnnext/dam/assets/181010131059-australia-best-beaches-cossies-beach-cocos3.jpg"
-                     alt="beach"/>
-            </div>
-            <div>ava+descr</div>
+            <ProfileInfo/>
             <MyPosts/>
         </div>
     )
